fbb2c3fa7bde4e91c75b4ef47ac4cc0a1a48a994
Christina <chri91eq@easv365.dk>
2021-04-06 16:03:53 +0200

    firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD_OBXdRcJ2j9AxXCrO5keR4yA9qvgjyxk",
+    authDomain: "tabtimize.firebaseapp.com",
+    databaseURL: "https://tabtimize-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "tabtimize",
+    storageBucket: "tabtimize.appspot.com",
+    messagingSenderId: "275081256152",
+    appId: "1:275081256152:web:0f3dafe3c3191f7b958bad",
+    measurementId: "G-4Y37QM8G7F"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
