acef6181f4f6990c95af630ff50f01be15a9a196
Christina <chri91eq@easv365.dk>
2021-05-19 03:24:59 +0200

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,86 +1,115 @@
 <template>
-  <div id="dashboard">
-    <section>
-      <v-row>
-        <v-col class="col-12">
-          <div class="profile">
-            <small>Welcome back</small>
-            <h2>{{ userProfile.name }}</h2>
-            <p>{{ userProfile.company }}</p>
-          </div>
-        </v-col>
-        <v-col class="col-12">
-          <v-sparkline
-            :value="value"
-            :gradient="gradient"
-            :smooth="radius || false"
-            :padding="padding"
-            :line-width="width"
-            :stroke-linecap="lineCap"
-            :gradient-direction="gradientDirection"
-            :fill="fill"
-            :type="type"
-            :auto-line-width="autoLineWidth"
-            auto-draw
-          ></v-sparkline>
-        </v-col>
-      </v-row>
-    </section>
-  </div>
+  <section id="dashboard">
+    <v-row>
+      <v-col cols="12">
+        <div class="profile">
+          <small>Welcome back</small>
+          <h2>{{ userProfile.name }}</h2>
+          <p>{{ userProfile.company }}</p>
+        </div>
+      </v-col>
+      <v-col cols="12" md="6" sm="12">
+        <v-card class="mt-4 mx-auto" max-width="400">
+          <v-sheet
+            class="v-sheet--offset mx-auto"
+            color="green"
+            elevation="12"
+            max-width="calc(100% - 32px)"
+          >
+            <v-sparkline
+              :labels="numbers"
+              :value="numberValue"
+              color="white"
+              line-width="2"
+              padding="16"
+            ></v-sparkline>
+          </v-sheet>
+
+          <v-card-text class="pt-0">
+            <div class="title font-weight-light mb-2">
+              Link Opportunities
+            </div>
+            <div class="subheading font-weight-light grey--text">
+              since last analyze
+            </div>
+            <v-divider class="my-2"></v-divider>
+            <v-icon class="mr-2" small>
+              mdi-clock
+            </v-icon>
+            <span class="caption grey--text font-weight-light"
+              >last analyzed 26 minutes ago</span
+            >
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" sm="12">
+        <v-card class="mt-4 mx-auto" max-width="400">
+          <v-sheet
+            class="v-sheet--offset mx-auto"
+            color="TabPurple"
+            elevation="12"
+            max-width="calc(100% - 32px)"
+          >
+            <v-sparkline
+              :labels="labels"
+              :value="value"
+              color="white"
+              line-width="2"
+              padding="16"
+            ></v-sparkline>
+          </v-sheet>
+
+          <v-card-text class="pt-0">
+            <div class="title font-weight-light mb-2">
+              User Registrations
+            </div>
+            <div class="subheading font-weight-light grey--text">
+              Last Campaign Performance
+            </div>
+            <v-divider class="my-2"></v-divider>
+            <v-icon class="mr-2" small>
+              mdi-clock
+            </v-icon>
+            <span class="caption grey--text font-weight-light"
+              >last registration 26 minutes ago</span
+            >
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </section>
 </template>
 
 <script>
 import { mapState } from "vuex";
 
-const gradients = [
-  ["#222"],
-  ["#42b3f4"],
-  ["red", "orange", "yellow"],
-  ["purple", "violet"],
-  ["#00c6ff", "#F0F", "#FF0"],
-  ["#f72047", "#ffd200", "#1feaea"],
-];
-
 export default {
-  components: {},
   data() {
     return {
-      width: 2,
-      radius: 10,
-      padding: 8,
-      lineCap: "round",
-      gradient: gradients[5],
-      value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
-      gradientDirection: "top",
-      gradients,
-      fill: false,
-      type: "trend",
-      autoLineWidth: false,
+      labels: ["12am", "3am", "6am", "9am", "12pm", "3pm", "6pm", "9pm"],
+      numbers: ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"],
+      numberValue: [200, 675, 410, 390, 310, 460, 250, 240, 200, 675, 410,],
+      value: [200, 675, 410, 390, 310, 460, 250, 240],
     };
   },
+
   computed: {
     ...mapState(["userProfile"]),
   },
-  methods: {},
-  filters: {},
+  methods: {
+    // profile picture test end ----------
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 #dashboard {
-  padding: 0;
-  // section {
-  //   padding: 2rem 0;
-  // }
-  /*.col1 {
-    flex: 0 0 30%;
-    @media screen and (max-width: 1000px) {
-      flex: 0 0 40%;
-    }
-  }*/
   .profile {
-    // background: $white;
     padding: 2rem;
+  }
+  .v-sheet--offset {
+    top: -24px;
+    position: relative;
   }
 }
 </style>
